5cd9b8ac9b44f84f9fc1e64af2fc1358a224e2c2
Wise <wisewebworks@outlook.com>
2025-01-17 22:37:02 +0100

    Initial setup project structure for image gallery.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/style.css';
+import Homepage from './pages/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+export default App;
